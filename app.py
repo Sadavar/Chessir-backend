988b491a43fe396cc03cf45f916dc9fe6f4d79eb
sadavar <sadavar@umich.edu>
2023-09-04 16:29:19 -0400

    fixing

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ def getTactics():
     result = []
     result.append(os.getcwd())
     result.append(os.listdir())
-    # result.append(os.path.realpath())
 
     response = jsonify(result)
 
@@ -31,6 +30,50 @@ def getTactics():
     game = chess.pgn.read_game(pgn)
     engine = chess.engine.SimpleEngine.popen_uci(os.getcwd() + "/stockfish-ubuntu")
 
+    board = game.board()
+
+    move_number = 1
+    turn_counter = 0
+    prev_score = 0
+    tactics = []
+
+    for move in game.mainline_moves():
+        board.push(move)
+        turn_counter += 1
+
+        info = engine.analyse(board, chess.engine.Limit(time=0.05))
+        curr_score = info["score"].black().score()
+        # print("Move:", move_number, move, "Score:", curr_score)
+        if curr_score is None:
+            continue
+
+        if turn_counter == 1:
+            diff = curr_score - prev_score
+            if diff > 200:
+                # print("Best: ", move)
+                move = info["pv"][0]
+
+                current_fen = board.fen()
+                board.push(move)
+                best_fen = board.fen()
+                board.pop()
+
+                puzzle = [current_fen, best_fen]
+                tactics.append(puzzle)
+
+        if turn_counter == 2:
+            prev_score = info["score"].black().score()
+            move_number += 1
+            turn_counter = 0
+
+    for tactic in tactics:
+        print(tactic[0])
+        print(tactic[1])
+
+
+    engine.quit()
+
+    response = jsonify(tactics)
     return response
 
 # print("python working!")
@@ -47,45 +90,45 @@ def getTactics():
 # # engine = chess.engine.SimpleEngine.popen_uci("stockfish")
 #
 # board = game.board()
-#
+
 # move_number = 1
 # turn_counter = 0
 # prev_score = 0
 # tactics = []
-#
+
 # for move in game.mainline_moves():
 #     board.push(move)
 #     turn_counter += 1
-#
+
 #     info = engine.analyse(board, chess.engine.Limit(time=0.05))
 #     curr_score = info["score"].black().score()
 #     # print("Move:", move_number, move, "Score:", curr_score)
 #     if curr_score is None:
 #         continue
-#
+
 #     if turn_counter == 1:
 #         diff = curr_score - prev_score
 #         if diff > 200:
 #             # print("Best: ", move)
 #             move = info["pv"][0]
-#
+
 #             current_fen = board.fen()
 #             board.push(move)
 #             best_fen = board.fen()
 #             board.pop()
-#
+
 #             puzzle = [current_fen, best_fen]
 #             tactics.append(puzzle)
-#
+
 #     if turn_counter == 2:
 #         prev_score = info["score"].black().score()
 #         move_number += 1
 #         turn_counter = 0
-#
+
 # for tactic in tactics:
 #     print(tactic[0])
 #     print(tactic[1])
-#
-#
+
+
 # engine.quit()
 # quit()
