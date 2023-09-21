80f2831ae1513714f8f4fedcc03a21692cd3539f
sadavar <sadavar@umich.edu>
2023-09-20 21:16:55 -0400

    changed to post req

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import io
 import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
+import json
 
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:3000", "https://chess-trainer-682560c6d2f7.herokuapp.com"])
@@ -16,19 +17,23 @@ CORS(app, origins=["http://localhost:3000", "https://chess-trainer-682560c6d2f7.
 def index():
     return "home"
 
-@app.route("/getTactics", methods=['GET'])
+@app.route("/getTactics", methods=['POST'])
 def getTactics():
     # result = ["tactic1", "tactic2", "tactic3"]
     result = []
     result.append(os.getcwd())
     result.append(os.listdir())
 
+    pgn = request.get_json()
+    
     response = jsonify(result)
 
-    pgn = request.args.get('pgns')
+    # pgn = request.args.get('pgns')
+    print("pgn: " + pgn)
     pgn = io.StringIO(pgn)
     game = chess.pgn.read_game(pgn)
     engine = chess.engine.SimpleEngine.popen_uci(os.getcwd() + "/stockfish-ubuntu")
+    # engine = chess.engine.SimpleEngine.popen_uci(os.getcwd() + '/stockfish')
 
     board = game.board()
 
@@ -73,62 +78,7 @@ def getTactics():
 
     engine.quit()
 
-    response = jsonify(output)
+    # response = jsonify(output)
+    print(response)
+    response = json.dumps(output)
     return response
-
-# print("python working!")
-# quit()
-#
-# data = open('src/main/python/pgn.txt', 'r').read()
-# # data = open('pgn.txt', 'r').read()
-# pgn = io.StringIO(data)
-# game = chess.pgn.read_game(pgn)
-#
-
-
-# engine = chess.engine.SimpleEngine.popen_uci("src/main/python/stockfish")
-# # engine = chess.engine.SimpleEngine.popen_uci("stockfish")
-#
-# board = game.board()
-
-# move_number = 1
-# turn_counter = 0
-# prev_score = 0
-# tactics = []
-
-# for move in game.mainline_moves():
-#     board.push(move)
-#     turn_counter += 1
-
-#     info = engine.analyse(board, chess.engine.Limit(time=0.05))
-#     curr_score = info["score"].black().score()
-#     # print("Move:", move_number, move, "Score:", curr_score)
-#     if curr_score is None:
-#         continue
-
-#     if turn_counter == 1:
-#         diff = curr_score - prev_score
-#         if diff > 200:
-#             # print("Best: ", move)
-#             move = info["pv"][0]
-
-#             current_fen = board.fen()
-#             board.push(move)
-#             best_fen = board.fen()
-#             board.pop()
-
-#             puzzle = [current_fen, best_fen]
-#             tactics.append(puzzle)
-
-#     if turn_counter == 2:
-#         prev_score = info["score"].black().score()
-#         move_number += 1
-#         turn_counter = 0
-
-# for tactic in tactics:
-#     print(tactic[0])
-#     print(tactic[1])
-
-
-# engine.quit()
-# quit()
