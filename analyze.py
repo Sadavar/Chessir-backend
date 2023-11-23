57f4a430b23a8b98ffba3930e211a287f6229c93
Varun Sadasivam <sadavar05@gmail.com>
2023-11-23 14:44:37 -0600

    init

--- analyze.py
+++ analyze.py
@@ -1,4 +1,3 @@
-#!/bin/sh
 import chess
 import chess.engine
 import chess.pgn
@@ -8,33 +7,37 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import json
 
-__name__ = "__analyze__"
-
-def getTactics2():
+def getTactics():
     puzzles = []
-    # read pgn 
+    # read request args
     pgn = open("pgn.txt", "r").read()
+    username = 'sadavar'
+
     pgn = io.StringIO(pgn)
-    
-    # configure game and engine
     game = chess.pgn.read_game(pgn)
+    headers = game.headers
+
+    # configure game and engine
     engine = chess.engine.SimpleEngine.popen_uci(os.getcwd() + '/stockfish')
     board = game.board()
 
-    move_counter = 1
+    user_turn = 'white' if username in headers.get("White") else 'black'
+
     prev_turn_score = 0
     turn = 'white'
-    
+    move_number = 0
     # simulate moves
     for move in game.mainline_moves():
+        move_number += 1
+        print("move: " + str(move) + ", move number: " + str(move_number))
         # get eval of position 
         start_fen = board.fen()
         info = engine.analyse(board, chess.engine.Limit(time=0.05))
+        if info["score"].white().score() is None:
+            continue
+
         before_score_white = info["score"].white().score()
         before_score_black = before_score_white * -1
-
-        if before_score_white is None or before_score_black is None:
-            continue
 
         # get best move
         best_move = info["pv"][0]
@@ -42,145 +45,60 @@ def getTactics2():
         board.push(move)
         # get eval after move
         info = engine.analyse(board, chess.engine.Limit(time=0.05))
+        if info["score"].white().score() is None:
+            continue
+    
         after_score_white = info["score"].white().score()
         after_score_black = after_score_white * -1
 
-        print("Move:", move_counter, ",", move, "Turn:", turn)
-        print("Before White Score: ", before_score_white, "Before Black Score:", before_score_black)
-        print("After White Score: ", after_score_white, "After Black Score:", after_score_black)
-
         tactic_threshold = 300
+        print("prev_turn_score: " + str(prev_turn_score) + ", turn: " + turn + ", user_turn: " + user_turn)
         # check if oppenent made a bad move
-        if(prev_turn_score > tactic_threshold):
+        if(prev_turn_score > tactic_threshold and turn == user_turn):
             # check if the tactic was missed
-            print("move: ", move, "best_move: ", best_move)
             board.pop()
             board.push(best_move)
             info = engine.analyse(board, chess.engine.Limit(time=0.05))
             board.pop()
+            # check if score is None
+            if info["score"].white().score() is None:
+                board.push(move)
+                continue
             
             # calculate the difference in score between the best move and the move played
             if(turn == 'white'):
                 best_score = info["score"].white().score()
                 best_move_diff = abs(best_score - after_score_white)
-            if(turn == 'black'):
+            else:
                 best_score = info["score"].black().score()
                 best_move_diff = abs(best_score - after_score_black)
                 
-            print("Best Score: ", best_score)
-
             best_move_diff_threshold = 50
             # check if the tactic was missed by threshold, a decent but not best move is allowed
+            print("best_move_diff: " + str(best_move_diff))
             if(best_move_diff > best_move_diff_threshold):
-                print("Tactic Missed----------------------------------------------------")
-                print("Best Move: ", best_move, "Best Score: ", best_score)
-
                 board.push(best_move)
                 best_fen = board.fen()
-                puzzle = [start_fen, best_fen]
-                puzzles.append(puzzle)
                 board.pop()
+                puzzle = [start_fen, best_fen, turn]
+                puzzles.append(puzzle)
             
             board.push(move)
             
-
-        if(turn == 'white'): turn = 'black'
-        else: turn = 'white'
-            
         if(turn == 'white'): 
             prev_turn_score = abs(after_score_white - before_score_white)
-        if(turn == 'black'): 
+            turn = 'black'
+        elif(turn == 'black'): 
             prev_turn_score = abs(after_score_black - before_score_black)
+            turn = 'white'
 
-        move_counter += 1
-    
     engine.quit()
-
-    # print out puzzles 
-    for puzzle in puzzles:
-        print(puzzle[0])
-        print(puzzle[1])
 
     response = json.dumps(puzzles)
     print(response)
     return response
 
+if __name__ == "__main__":
+    getTactics()
+    print("done")
 
-def getTactics():
-    # result = ["tactic1", "tactic2", "tactic3"]
-    result = []
-    result.append(os.getcwd())
-    result.append(os.listdir())
-
-    # read pgn from pgn.txt
-    pgn = open("pgn.txt", "r").read()    
-    # pgn = request.get_json()
-    
-    # response = jsonify(result)
-
-
-    # pgn = request.args.get('pgns')
-    print("pgn: " + pgn)
-    pgn = io.StringIO(pgn)
-    game = chess.pgn.read_game(pgn)
-    # engine = chess.engine.SimpleEngine.popen_uci(os.getcwd() + "/stockfish-ubuntu")
-    engine = chess.engine.SimpleEngine.popen_uci(os.getcwd() + '/stockfish')
-
-    board = game.board()
-
-    move_number = 1
-    turn_counter = 0
-    prev_score = 0
-    tactics = []
-    output = []
-
-    for move in game.mainline_moves():
-        board.push(move)
-        turn_counter += 1
-
-        info = engine.analyse(board, chess.engine.Limit(time=0.05))
-        curr_score = info["score"].black().score()
-        # print("Move:", move_number, move, "Score:", curr_score)
-
-
-
-        if curr_score is None:
-            continue
-
-        if turn_counter == 1:
-            diff = curr_score - prev_score
-            if diff > 200:
-                # print("Best: ", move)
-                move = info["pv"][0]
-
-                current_fen = board.fen()
-                board.push(move)
-                best_fen = board.fen()
-                board.pop()
-
-                puzzle = [current_fen, best_fen]
-                tactics.append(puzzle)
-
-        if turn_counter == 2:
-            prev_score = info["score"].black().score()
-            move_number += 1
-            turn_counter = 0
-
-    for tactic in tactics:
-        output.append(tactic[0])
-        output.append(tactic[1])
-
-    engine.quit()
-
-    # response = jsonify(output)
-    response = json.dumps(output)
-    print(response)
-
-    return response
-
-def main():
-
-    getTactics2()
-
-if __name__ == "__analyze__":
-    main()
--- app.py
+++ app.py
@@ -28,26 +28,26 @@ def getTactics():
     game = chess.pgn.read_game(pgn)
     headers = game.headers
 
-    
     # configure game and engine
-    engine = chess.engine.SimpleEngine.popen_uci(os.getcwd() + '/stockfish-ubuntu')
+    engine = chess.engine.SimpleEngine.popen_uci(os.getcwd() + '/stockfish')
     board = game.board()
 
-    user_turn = 'white'
-    if username in headers.get("White"):
-        user_turn = 'white'
-    else: user_turn = 'black'
+    user_turn = 'white' if username in headers.get("White") else 'black'
 
     prev_turn_score = 0
     turn = 'white'
+    move_number = 0
     # simulate moves
     for move in game.mainline_moves():
+        move_number += 1
+        print("move: " + str(move) + ", move number: " + str(move_number))
         # get eval of position 
         start_fen = board.fen()
         info = engine.analyse(board, chess.engine.Limit(time=0.05))
-        before_score_white = info["score"].white().score()
-        if before_score_white is None:
+        if info["score"].white().score() is None:
             continue
+
+        before_score_white = info["score"].white().score()
         before_score_black = before_score_white * -1
 
         # get best move
@@ -56,12 +56,14 @@ def getTactics():
         board.push(move)
         # get eval after move
         info = engine.analyse(board, chess.engine.Limit(time=0.05))
-        after_score_white = info["score"].white().score()
-        if after_score_white is None:
+        if info["score"].white().score() is None:
             continue
+    
+        after_score_white = info["score"].white().score()
         after_score_black = after_score_white * -1
 
         tactic_threshold = 300
+        print("prev_turn_score: " + str(prev_turn_score) + ", turn: " + turn + ", user_turn: " + user_turn)
         # check if oppenent made a bad move
         if(prev_turn_score > tactic_threshold and turn == user_turn):
             # check if the tactic was missed
@@ -84,6 +86,7 @@ def getTactics():
                 
             best_move_diff_threshold = 50
             # check if the tactic was missed by threshold, a decent but not best move is allowed
+            print("best_move_diff: " + str(best_move_diff))
             if(best_move_diff > best_move_diff_threshold):
                 board.push(best_move)
                 best_fen = board.fen()
@@ -92,14 +95,14 @@ def getTactics():
                 puzzles.append(puzzle)
             
             board.push(move)
-
-        if(turn == 'white'): turn = 'black'
-        else: turn = 'white'
-            
-        if(turn == 'white'): 
-            prev_turn_score = abs(after_score_white - before_score_white)
-        if(turn == 'black'): 
-            prev_turn_score = abs(after_score_black - before_score_black)
+        
+        match turn:
+            case 'white':
+                prev_turn_score = abs(after_score_white - before_score_white)
+                turn = 'black'
+            case 'black':
+                prev_turn_score = abs(after_score_black - before_score_black)
+                turn = 'white'
 
     engine.quit()
 
