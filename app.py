7e05e1dfda6900260b17d011e8bb90d3221f6db2
Varun Sadasivam <sadavar05@gmail.com>
2024-01-03 22:09:00 -0600

    cors

--- app.py
+++ app.py
@@ -12,6 +12,14 @@ import json
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 
+@app.after_request
+def add_headers(response):
+    response.headers.add('Content-Type', 'application/json')
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Expose-Headers', 'Content-Type,Content-Length,Authorization,X-Pagination')
+    return response
 
 @app.route("/")
 def index():
